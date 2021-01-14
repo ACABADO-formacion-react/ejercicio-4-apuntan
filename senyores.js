1b3ceaa6954df011779505856bf4e4a990b9e19b
Mario <mario@mariogl.com>
2021-01-14 15:42:11 +0100

    Commit inicial

---
+++ senyores.js
@@ -0,0 +1,26 @@
+const senyores = [
+    {
+        id: 1,
+        nombre: "El Fary",
+        profesion: "Influencer",
+        estado: "RIP",
+        twitter: "pendiente",
+        imagen: "fary.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Julio Iglesias",
+        profesion: "Youtuber",
+        estado: "Vivo",
+        twitter: "@JulioIglesias",
+        imagen: "julio.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Bertín Osborne",
+        profesion: "Java Developer",
+        estado: "Vivo",
+        twitter: "@BertinOsborne",
+        imagen: "bertin.jpg"
+    }
+]
